70733b00aa65cabd911a398327735a4d7f6b1323
Griffin Moede <griffinmoede@gmail.com>
2024-11-11 21:58:08 -0800

    Griffin tries nextjs gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC, ReactElement } from "react";
+import GalleryWall from "../../components/gallerywall/GalleryWall";
+
+const Gallery: FC = (): ReactElement => {
+  return (
+    <>
+      <GalleryWall>
+      </GalleryWall>
+    </>
+  );
+};
+
+export default Gallery;
